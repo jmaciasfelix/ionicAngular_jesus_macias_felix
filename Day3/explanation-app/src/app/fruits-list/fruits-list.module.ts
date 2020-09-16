686c8a1b0d28ab360bf339674165b7d96c93f3a0
Jesus Macias <maciasfelixj@gmail.com>
2020-09-16 20:27:23 +0200

    Add Day3 with exercises

---
+++ Day3/explanation-app/src/app/fruits-list/fruits-list.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { FruitsListPage } from './fruits-list.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: FruitsListPage
+      }
+    ])
+  ],
+  declarations: [FruitsListPage]
+})
+export class FruitsListPageModule {}
