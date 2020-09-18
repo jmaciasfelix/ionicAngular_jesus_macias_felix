59d34ce82c4268cb1b3e862a5cbdbfba893e02d4
Jesus Macias <maciasfelixj@gmail.com>
2020-09-18 19:24:50 +0200

    Add buttons rating

--- Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
+++ Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sandbox',
-  templateUrl: './sandbox.page.html',
-  styleUrls: ['./sandbox.page.scss'],
+  selector: "app-sandbox",
+  templateUrl: "./sandbox.page.html",
+  styleUrls: ["./sandbox.page.scss"],
 })
 export class SandboxPage implements OnInit {
+  public like: number;
+  public dislike: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.like = 1;
+    this.dislike = 0;
   }
-
 }
