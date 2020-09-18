508f14e268dbfae8c5ee3aeef606e2cc0dd90b92
Jesus Macias <maciasfelixj@gmail.com>
2020-09-18 17:33:33 +0200

    Restore default fruit list

--- Day3/explanation-app/src/app/fruits-list/fruits-list.page.ts
+++ Day3/explanation-app/src/app/fruits-list/fruits-list.page.ts
@@ -1,86 +1,103 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { Fruit } from '../models/fruit.model';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { Fruit } from "../models/fruit.model";
 
 @Component({
-  selector: 'app-fruits-list-page',
-  templateUrl: 'fruits-list.page.html',
-  styleUrls: ['fruits-list.page.scss'],
+  selector: "app-fruits-list-page",
+  templateUrl: "fruits-list.page.html",
+  styleUrls: ["fruits-list.page.scss"],
 })
 export class FruitsListPage implements OnInit {
-
   public fruits: Fruit[];
+  public defaultFruits: Fruit[];
+  public isRestoreDisable: boolean = true;
 
   /**
    * constructor
    *
    * @param navController ionic-angular nav controller
    */
-  constructor(private readonly navController: NavController) { }
+  constructor(private readonly navController: NavController) {}
 
   public ngOnInit(): void {
-
     this.fruits = [
       {
         id: 1,
-        name: 'Apple',
-        image: 'apple.png',
-        description: 'The usually round red, green, or yellow fruit of a small tree of the rose family.',
-        link: 'https://www.wikipedia.org/wiki/Apple'
+        name: "Apple",
+        image: "apple.png",
+        description:
+          "The usually round red, green, or yellow fruit of a small tree of the rose family.",
+        link: "https://www.wikipedia.org/wiki/Apple",
       },
       {
         id: 2,
-        name: 'Orange',
-        image: 'orange.png',
-        description: 'A rounded, reddish yellow, bitter or sweet citrus fruit that can be eaten.',
+        name: "Orange",
+        image: "orange.png",
+        description:
+          "A rounded, reddish yellow, bitter or sweet citrus fruit that can be eaten.",
       },
       {
         id: 3,
-        name: 'Lemon',
-        image: 'lemon.png',
-        description: 'The yellowish, acid fruit of a subtropical citrus tree.',
-        link: 'https://www.wikipedia.org/wiki/Lemon'
+        name: "Lemon",
+        image: "lemon.png",
+        description: "The yellowish, acid fruit of a subtropical citrus tree.",
+        link: "https://www.wikipedia.org/wiki/Lemon",
       },
       {
         id: 4,
-        name: 'Pear',
-        image: 'pear.png',
-        description: 'The rounded fruit of a tree of the rose family.',
+        name: "Pear",
+        image: "pear.png",
+        description: "The rounded fruit of a tree of the rose family.",
       },
       {
         id: 5,
-        name: 'Strawberry',
-        image: 'strawberry.png',
-        description: 'The fleshy red fruit of a stemless plant belonging to the rose family.',
-        link: 'https://www.wikipedia.org/wiki/Strawberry'
+        name: "Strawberry",
+        image: "strawberry.png",
+        description:
+          "The fleshy red fruit of a stemless plant belonging to the rose family.",
+        link: "https://www.wikipedia.org/wiki/Strawberry",
       },
       {
         id: 6,
-        name: 'Pineapple',
-        image: 'pineapple.png',
-        description: 'A tropical plant having a short stem and rigid, spiny leaves.',
+        name: "Pineapple",
+        image: "pineapple.png",
+        description:
+          "A tropical plant having a short stem and rigid, spiny leaves.",
       },
       {
         id: 7,
-        name: 'Watermelon',
-        image: 'watermelon.png',
-        description: 'A large melon with a hard, green rind and sweet, juicy, usually red pulp.',
-        link: 'https://www.wikipedia.org/wiki/Watermelon'
+        name: "Watermelon",
+        image: "watermelon.png",
+        description:
+          "A large melon with a hard, green rind and sweet, juicy, usually red pulp.",
+        link: "https://www.wikipedia.org/wiki/Watermelon",
       },
       {
         id: 8,
-        name: 'Grapes',
-        image: 'grapes.png',
-        description: 'The smooth-skinned, green or purple fruit that grows in clusters on vines, may be eaten, and is used to make wine.',
-      }
+        name: "Grapes",
+        image: "grapes.png",
+        description:
+          "The smooth-skinned, green or purple fruit that grows in clusters on vines, may be eaten, and is used to make wine.",
+      },
     ];
+    this.defaultFruits = [...this.fruits];
   }
 
   public sandboxClick(): void {
-    console.log('TODO: Navigate to sandbox page');
+    console.log("TODO: Navigate to sandbox page");
   }
-
+  /**
+   * delete fruit from screen
+   * @param index fruit array index
+   */
   public deleteFruit(index: number): void {
-    console.log('TODO: Remove fruit index', index);
+    this.isRestoreDisable = false;
+  }
+  /**
+   * restore list of fruits
+   */
+  public restoreFruits(): void {
+    this.isRestoreDisable = true;
+    this.fruits = [...this.defaultFruits];
   }
 }
