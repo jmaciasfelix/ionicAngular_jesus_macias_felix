c08094f18f40b052c847eb945ab1f0f520943250
Jesus Macias <maciasfelixj@gmail.com>
2020-09-19 11:13:09 +0200

    Like and dislike system rating

--- Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
+++ Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
@@ -8,9 +8,34 @@ import { Component, OnInit } from "@angular/core";
 export class SandboxPage implements OnInit {
   public like: number;
   public dislike: number;
+  public meanRating: number;
 
   public ngOnInit(): void {
-    this.like = 1;
+    this.like = 0;
     this.dislike = 0;
+    this.meanRating = 0;
+  }
+  /**
+   * Add a like
+   */
+  public likeClick(): void {
+    this.like++;
+    this.updateMeanRating();
+  }
+
+  /**
+   * Add a dislike
+   */
+  public dislikeClick(): void {
+    this.dislike++;
+    this.updateMeanRating();
+  }
+
+  /**
+   * Update the average of the ranking
+   */
+  private updateMeanRating(): void {
+    const numVote: number = this.like + this.dislike;
+    this.meanRating = parseFloat((10*(this.like / numVote)).toFixed(1));
   }
 }
