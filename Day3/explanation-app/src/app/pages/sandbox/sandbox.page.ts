047d9302d38fe6b92ba51303803b782317f8b6e4
Jesus Macias <maciasfelixj@gmail.com>
2020-09-19 12:24:40 +0200

    Logic expandle content and user form

--- Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
+++ Day3/explanation-app/src/app/pages/sandbox/sandbox.page.ts
@@ -9,11 +9,15 @@ export class SandboxPage implements OnInit {
   public like: number;
   public dislike: number;
   public meanRating: number;
+  public isHidden: boolean;
+  public iconBtn: string;
 
   public ngOnInit(): void {
     this.like = 0;
     this.dislike = 0;
     this.meanRating = 0;
+    this.isHidden = true;
+    this.iconBtn = "add";
   }
   /**
    * Add a like
@@ -31,11 +35,17 @@ export class SandboxPage implements OnInit {
     this.updateMeanRating();
   }
 
+  public handleExpandableContent(): void {
+    console.log("handleExpandableContent");
+    this.isHidden = !this.isHidden;
+    this.isHidden ? (this.iconBtn = "add") : (this.iconBtn = "remove");
+  }
+
   /**
    * Update the average of the ranking
    */
   private updateMeanRating(): void {
     const numVote: number = this.like + this.dislike;
-    this.meanRating = parseFloat((10*(this.like / numVote)).toFixed(1));
+    this.meanRating = parseFloat((10 * (this.like / numVote)).toFixed(1));
   }
 }
