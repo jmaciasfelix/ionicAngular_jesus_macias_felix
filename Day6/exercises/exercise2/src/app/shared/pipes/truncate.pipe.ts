20e82d8dbd2bae6220f85c4b105332f544df8bc0
Jesus Macias <maciasfelixj@gmail.com>
2020-09-23 20:51:21 +0200

    Truncate for words

--- Day6/exercises/exercise2/src/app/shared/pipes/truncate.pipe.ts
+++ Day6/exercises/exercise2/src/app/shared/pipes/truncate.pipe.ts
@@ -1,3 +1,4 @@
+//angular
 import { Pipe, PipeTransform, Injectable } from "@angular/core";
 
 @Pipe({
@@ -6,15 +7,47 @@ import { Pipe, PipeTransform, Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-
 export class TruncatePipe implements PipeTransform {
-  transform(text: string, n: number = 0): string {
-    const arrayStr = text.split(" ");
+  /**
+   * Truncate a array for number of letter.
+   * @param arrayStr Text words list
+   * @param limit Limit
+   */
+  truncateLetters = (arrayStr: string[], limit: number) => {
     let newStr = "";
     arrayStr.forEach((elemento) => {
-      newStr.length < n ? (newStr += " " + elemento) : null;
+      newStr.length < limit ? (newStr += " " + elemento) : null;
     });
     newStr += "...";
+    return newStr;
+  };
+  /**
+   * Truncate a array for number of words.
+   * @param arrayStr Text words list
+   * @param limit Limit
+   */
+  truncateWords = (arrayStr: string[], limit: number) => {
+    let newStr = "";
+    arrayStr.forEach((elemento, index) => {
+      index < limit ? (newStr += " " + elemento) : null;
+    });
+    newStr += "...";
+    return newStr;
+  };
+  transform(text: string, n: number = 0, words: string = ""): string {
+    let newStr = "";
+    const arrayStr = text.split(" ");
+
+    if (n !== 0) {
+      if (words === "words") {
+        newStr = this.truncateWords(arrayStr, n);
+      } else {
+        newStr = this.truncateLetters(arrayStr, n);
+      }
+    } else {
+      newStr = text;
+    }
+
     return newStr;
   }
 }
