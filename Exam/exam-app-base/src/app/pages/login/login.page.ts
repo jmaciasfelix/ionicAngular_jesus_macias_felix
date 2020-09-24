c4995177c6d9255fbca7eb929bbf7d40e3174299
Jesus Macias <maciasfelixj@gmail.com>
2020-09-24 17:05:29 +0200

    feat: reactive form

--- Exam/exam-app-base/src/app/pages/login/login.page.ts
+++ Exam/exam-app-base/src/app/pages/login/login.page.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/shared/services/auth.service';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/shared/services/auth.service";
+import { AuthCredentials } from "../../shared/models";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss']
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage {
-  constructor(private authService: AuthService, private router: Router) {}
+  public form: FormGroup = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(5)]],
+  });
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   public performAuth(): void {
-    this.authService.login({ email: 'smoro@atsistemas.com', password: 'smoro' }).subscribe(
+    const { email, password }: AuthCredentials = this.form.value;
+
+    this.authService.login({ email: email, password: password }).subscribe(
       () => {
-        console.log('LOGIN OK');
-        this.router.navigate(['/surveys-list']);
+        console.log("LOGIN OK");
+        this.router.navigate(["/surveys-list"]);
       },
       (error) => {
-        console.log('LOGIN ERROR', error);
+        console.log("LOGIN ERROR", error);
       }
     );
   }
