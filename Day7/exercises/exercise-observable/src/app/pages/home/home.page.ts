75454ab00d3a5233a6ac75eb02a1e90b1814e9cd
Jesus Macias <maciasfelixj@gmail.com>
2020-09-24 10:53:36 +0200

    feat: ngFor

--- Day7/exercises/exercise-observable/src/app/pages/home/home.page.ts
+++ Day7/exercises/exercise-observable/src/app/pages/home/home.page.ts
@@ -1,27 +1,29 @@
-import { Component } from '@angular/core';
-import { PostService } from 'src/app/shared/services/post.service';
+import { Component } from "@angular/core";
+import { PostService } from "src/app/shared/services/post.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  public messages: string[] = ["msg1","msg2","msg3"];
 
   constructor(private postService: PostService) {}
 
   /** TODO
-   * 
+   *
    */
-  public loadPosts(){
+  public loadPosts() {
     console.log("TODO: LoadPost");
     this.postService.loadPosts();
   }
 
   /** TODO
-   * 
+   *
    */
-  public clearList(){
+  public clearList() {
     console.log("TODO: clearList");
+    this.messages = [];
   }
 }
