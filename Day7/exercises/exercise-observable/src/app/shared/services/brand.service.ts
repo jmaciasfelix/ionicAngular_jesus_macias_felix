079d91a372cc4821aae311ad91035cf24274e438
Jesus Macias <maciasfelixj@gmail.com>
2020-09-26 11:02:14 +0200

    Clean code services

--- Day7/exercises/exercise-observable/src/app/shared/services/brand.service.ts
+++ Day7/exercises/exercise-observable/src/app/shared/services/brand.service.ts
@@ -1,7 +1,11 @@
+//angular
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+// rxjs
+import { Observable } from "rxjs";
+//environment
+import { environment } from "src/environments/environment";
+//models
 import { Brands } from "../models";
 
 @Injectable({
@@ -9,12 +13,17 @@ import { Brands } from "../models";
 })
 export class BrandService {
   public brands: Brands[];
+  /**
+   * Contructor
+   * @param httpClient HttpClient angular
+   */
   constructor(private httpClient: HttpClient) {}
 
-  /** TODO
-   *  
+  /**
+   *  Observable to get brands
+   * @return Observable Brands[]
    */
-  getBrands(): Observable<Brands[]>{
+  getBrands(): Observable<Brands[]> {
     return this.httpClient.get<Brands[]>(`${environment.apiUrl}/brands`);
   }
 }
--- Day7/exercises/exercise-observable/src/app/shared/services/post.service.ts
+++ Day7/exercises/exercise-observable/src/app/shared/services/post.service.ts
@@ -3,21 +3,17 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Post, Brands } from "../models";
-import { BrandService } from "./brand.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class PostService {
   public brands: Brands[];
-  public posts;
-  constructor(
-    private httpClient: HttpClient,
-    private brandService: BrandService
-  ) {}
+  constructor(private httpClient: HttpClient) {}
 
-  /** TODO
-   *
+  /**
+   *  Observable to get posts
+   * @return Observable Post[]
    */
   getPost(): Observable<Post[]> {
     return this.httpClient.get<Post[]>(`${environment.apiUrl}/posts`);
