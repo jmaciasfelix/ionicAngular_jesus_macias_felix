bf7e8007f5d2fc0443becaae3b3914210c29fad5
Jesus Macias <maciasfelixj@gmail.com>
2020-09-27 18:45:52 +0200

    feat: translate service in toast msg

--- Day7/exercises/exercise-app-crud/src/app/pages/books/books.page.ts
+++ Day7/exercises/exercise-app-crud/src/app/pages/books/books.page.ts
@@ -1,7 +1,15 @@
+//angular
 import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+
+//ionic
+import { ToastController } from "@ionic/angular";
+
+//services
 import { BookService } from "src/app/shared/services/book.service";
+
+//models
 import { Book } from "src/app/shared/models";
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: "app-books",
@@ -12,17 +20,35 @@ export class BooksPage {
   state = "loading";
   books: Book[];
 
-  constructor(private bookService: BookService, public toastController: ToastController) {}
+  /**
+   * Contructor
+   * @param bookService Book service
+   * @param toastController Toast controller angular
+   * @param translateService Translate service angular
+   */
+  constructor(
+    private bookService: BookService,
+    public toastController: ToastController,
+    private translateService: TranslateService
+  ) {}
 
-  async presentToast(msg:string, color:string) {
+  /**
+   * Display a message for 2 seconds
+   * @param msg Message display in toast
+   * @param color Color toast
+   */
+  async presentToast(msg: string, color: string) {
     const toast = await this.toastController.create({
       message: msg,
       color: color,
-      duration: 2000
+      duration: 2000,
     });
     toast.present();
   }
 
+  /**
+   * ion view will enter ionic life cycle method
+   */
   ionViewWillEnter() {
     this.loadBooks();
   }
@@ -43,19 +69,28 @@ export class BooksPage {
     );
   }
 
-  deleteBook(idBook: string) {
-    console.log(idBook);
-    const idNumberBook:number = parseInt(idBook,10);
-     this.state = 'loading';
-     this.bookService.deleteBook(idNumberBook).subscribe(
-       () => {
-         this.ionViewWillEnter();
-         this.presentToast(`The book has been successfully removed.`, "success");
-       },
-       () => {
-         this.state = 'error';
-         this.presentToast(`An error occurred while deleting the book.`, "danger");
-       }
-     );
+  /**
+   * Delete a book and reload the list
+   * @param idBook id book to delete
+   */
+  deleteBook(idBook: string): void{
+    const idNumberBook: number = parseInt(idBook, 10);
+    this.state = "loading";
+    this.bookService.deleteBook(idNumberBook).subscribe(
+      () => {
+        this.ionViewWillEnter();
+        this.presentToast(
+          this.translateService.instant("MSGS.DELETING_OK"),
+          "success"
+        );
+      },
+      () => {
+        this.state = "error";
+        this.presentToast(
+          this.translateService.instant("ERRORS.DELETING"),
+          "danger"
+        );
+      }
+    );
   }
 }
